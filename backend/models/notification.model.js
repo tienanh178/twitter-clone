fdfaab387da0999f3a15e86cf917aad1fc735855
Tien Anh <phamtienanh2001@gmail.com>
2024-12-13 22:11:10 +0700

    1-user routes and controllers added

---
+++ backend/models/notification.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import User from "./user.model.js";
+
+const notificationSchema = mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["follow", "like"],
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamp: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export default Notification;
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -40,6 +40,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    coverImg: {
+      type: String,
+      default: "",
+    },
     bio: {
       type: String,
       default: "",
